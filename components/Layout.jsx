fd9381cbf8fadbb44e3394c4120e0c383580d708
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-26 14:29:39 +0200

    Add the fonts from Google Fonts and the normalize file.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,26 @@
 import Header from "./Header";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Product Hunt Clone - NextJS y Firebase</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <Header />
 
       <main>{children}</main>
