b369495f1b8e04674974d41b01ecc420dc92ebfb
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-26 13:19:39 +0200

    Study routing in Next.JS

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 const Layout = ({ children }) => {
   return (
     <>
       <h1>Header</h1>
+
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/aboutus">About us</Link>
+      </nav>
+
       <main>{children}</main>
     </>
   );
---
+++ pages/aboutus.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Aboutus = () => {
+  return (
+    <Layout>
+      <p>About us</p>
+    </Layout>
+  );
+};
+
+export default Aboutus;
