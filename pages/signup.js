da3adcb6554ef104db626a758d4b493c4bdc6b81
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-27 13:34:39 +0200

    Define the sign up form.

--- pages/signup.js
+++ pages/signup.js
@@ -3,7 +3,34 @@ import Layout from "../components/Layout";
 export default function Signup() {
   return (
     <Layout>
-      <p>Sign Up</p>
+      <>
+        <h1>Sign Up</h1>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" placeholder="Your Name" name="name" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Your Email"
+              name="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Your Password"
+              name="password"
+            />
+          </div>
+          <input type="submit" value="Create account"/>
+        </form>
+      </>
     </Layout>
   );
 }
