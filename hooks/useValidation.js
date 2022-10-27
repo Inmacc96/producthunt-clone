afb9866c9badb0cc933404f4a5f529c50f467b44
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-27 14:18:27 +0200

    Create a custom hook for login and sign up form validation.

---
+++ hooks/useValidation.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+const useValidation = (initialState, validate, fnSubmit) => {
+  //initialState: el estado inicial
+  //validate: Lo que vamos a validar
+  //fnSubmit: Función que se va a ejecutar cuando se haga el submit
+
+  //Los valores que el usuario introduzca en los inputs
+  const [data, setData] = useState(initialState);
+  const [error, setError] = useState({});
+  const [submitForm, setSubmitForm] = useState(false);
+
+  useEffect(() => {
+    if (submitForm) {
+      //Comprueba si existe errores
+      const noErrors = Object.keys(error).length == 0;
+
+      //Si no hay errores, entonces ejecuta la función del submit
+      if (noErrors) {
+        fnSubmit();
+      }
+
+      setSubmitForm(false);
+    }
+  }, []);
+
+  //Función que se ejecuta conforme el usuario escribe algo
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Función que se ejecuta cuando el usuario hace submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errorsValidation = validate(data);
+    setError(errorsValidation);
+    setSubmitForm(true);
+  };
+
+  return {
+    data,
+    error,
+    submitForm,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useValidation;
+
+//Este hook va a esperar que submitForm sea true para ejecutarse
