e70679a2cb5fc43c7e42dbb3cbb34154e84ddc27
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-01 21:35:28 +0100

    Delete ;

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <FirebaseContext.Provider value={{ register }}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </FirebaseContext.Provider>
   );
 }
