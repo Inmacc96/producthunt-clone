7f23c0b7de1b0d0d88fcf701b54448cf71a593e7
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-02 20:46:09 +0100

    Define the new Product form.

--- pages/newProduct.js
+++ pages/newProduct.js
@@ -1,9 +1,136 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
+import styles from "../styles/Form.module.css";
+
+//Validaciones
+import useValidation from "../hooks/useValidation";
+import validateSignUp from "../validation/validateSignUp";
+
+const INITIAL_STATE = {
+  name: "",
+  company: "",
+  image: "",
+  url: "",
+  description: "",
+};
 
 export default function NewProduct() {
+  const [errorNewProduct, setErrorNewProduct] = useState("");
+
+  const { data, error, handleChange, handleSubmit, handleBlur } = useValidation(
+    INITIAL_STATE,
+    validateSignUp,
+    createNewProduct
+  );
+
+  const { name, company, image, url, description } = data;
+
+  async function createNewProduct() {}
+
   return (
     <Layout>
-      <p>New product</p>
+      <>
+        <h1>New Product</h1>
+        <form className={styles.form} onSubmit={handleSubmit} noValidate>
+          <fieldset className={styles.fieldset}>
+            <legend>General Information</legend>
+
+            <div className={styles.field}>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Your Name"
+                name="name"
+                value={name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+
+            {error.name && <p className={styles.error}>{error.name}</p>}
+
+            <div className={styles.field}>
+              <label htmlFor="company">Company</label>
+              <input
+                type="text"
+                id="company"
+                placeholder="Company Name"
+                name="company"
+                value={company}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+
+            {error.company && <p className={styles.error}>{error.company}</p>}
+
+            <div className={styles.field}>
+              <label htmlFor="image">Image</label>
+              <input
+                type="file"
+                id="image"
+                name="image"
+                value={image}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+
+            {error.image && <p className={styles.error}>{error.image}</p>}
+
+            <div className={styles.field}>
+              <label htmlFor="url">Url</label>
+              <input
+                type="url"
+                id="url"
+                name="url"
+                value={url}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+
+            {error.url && <p className={styles.error}>{error.url}</p>}
+          </fieldset>
+
+          <fieldset className={styles.fieldset}>
+            <legend>About your Product</legend>
+
+            <div className={styles.field}>
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                name="description"
+                value={description}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+
+            {error.description && (
+              <p className={styles.error}>{error.description}</p>
+            )}
+          </fieldset>
+
+          <input
+            type="submit"
+            value="Create Product"
+            className={styles.inputSubmit}
+          />
+
+          {errorNewProduct && <p className={styles.error}>{errorNewProduct}</p>}
+        </form>
+
+        <style jsx>
+          {`
+            h1 {
+              text-align: center;
+              margin-top: 5rem;
+            }
+          `}
+        </style>
+      </>
     </Layout>
   );
 }
