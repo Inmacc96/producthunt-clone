ac2905093d18892d7a23f6b4c409e8ffb2e6f0ee
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-04 21:09:41 +0100

    Define the HTML structure of ProductDetails.

--- components/ProductDetails.jsx
+++ components/ProductDetails.jsx
@@ -1,3 +1,6 @@
+import styles from "../styles/ProductDetails.module.css";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const ProductDetails = ({ product }) => {
   const {
     id,
@@ -10,16 +13,34 @@ const ProductDetails = ({ product }) => {
     image,
     votes,
   } = product;
+
   return (
     <li>
+      {/* Description */}
       <div>
-        <div></div>
+        <div>
+          <img src={image} className={styles.img} />
+        </div>
 
         <div>
           <h1>{name}</h1>
+
+          <p>{description}</p>
+
+          <div>
+            <img src="/static/img/comment.png" />
+            <p>{comments.length} Comments</p>
+          </div>
+
+          <p>Published ago: {formatDistanceToNow(new Date(created))}</p>
         </div>
       </div>
-      <div></div>
+
+      {/*  Votes */}
+      <div>
+        <div>&#9650;</div>
+        <p>{votes}</p>
+      </div>
     </li>
   );
 };
