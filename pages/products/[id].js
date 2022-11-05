02a6d6ebe7e8c1182fa5da1965a598661c80fec1
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-05 16:05:55 +0100

    Get id from the route.

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 const Product = () => {
-  return <h1>Desde [id].js</h1>;
+  //Routing para obtener el id actual
+  const router = useRouter();
+  const {
+    query: { id },
+  } = router;
+
+  useEffect(() => {
+    //Nos vamos a esperar a que haya un id para realizar la consulta
+    if (id) {
+      console.log("Ya hay un id", id);
+    }
+  }, [id]);
+  return <h1>Desde {id}.js</h1>;
 };
 
 export default Product;
