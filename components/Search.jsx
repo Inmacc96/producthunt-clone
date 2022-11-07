240724b84dfa0e14bf313c55b3dfa72656bc796e
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-07 18:02:10 +0100

    Save the search in a state and send it to the search page using the path.

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import Route from "next/router";
 import styles from "../styles/Search.module.css";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  const searchProduct = (e) => {
+    e.preventDefault();
+
+    if (search.trim() === "") return;
+
+    // Redireccionar a /search
+    Route.push({
+      pathname: "/search",
+      query: { q: search },
+    });
+  };
   return (
-    <form className={styles.formSearch}>
-      <input type="text" className={styles.inputText}
-      placeholder="Search Products" />
+    <form className={styles.formSearch} onSubmit={searchProduct}>
+      <input
+        type="text"
+        className={styles.inputText}
+        placeholder="Search Products"
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
       <button type="submit" className={styles.inputSubmit}>
         Search
--- pages/search.js
+++ pages/search.js
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function Search() {
+  const route = useRouter();
+
+  const {
+    query: { q },
+  } = route;
+
+  console.log(q);
   return <p>Search</p>;
 }
