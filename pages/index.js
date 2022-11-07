e6ae97b384b33e696ae90b846337642995be0c66
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-07 17:45:48 +0100

    Use useProducts in the home page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
-import { useEffect, useState } from "react";
 import ProductDetails from "../components/ProductDetails";
-import { getData } from "../firebase";
+import useProducts from "../hooks/useProducts";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    // Obtener los productos de la base de datos de Firebase
-    const getProducts = async () => {
-      const results = await getData();
-
-      const updatedProducts = results.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      setProducts(updatedProducts);
-    };
-
-    getProducts();
-  }, []);
+  const { products } = useProducts("created");
   return (
     <div className={styles.listProducts}>
       <div className={styles.container}>
