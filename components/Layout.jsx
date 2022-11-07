12eb835de6111cdbafb087d029de8ef9903dabe5
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-07 18:26:44 +0100

    Fix link crossorigin property name

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -14,7 +14,7 @@ const Layout = ({ children }) => {
           referrerPolicy="no-referrer"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
           rel="stylesheet"
