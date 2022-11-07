dee9c43b88e0300b7160c511babff83fc9bb765b
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-07 18:15:30 +0100

    - Define the search page - Filter the products with the search

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import useProducts from "../hooks/useProducts";
+import ProductDetails from "../components/ProductDetails";
+import styles from  "../styles/Home.module.css";
 
 export default function Search() {
   const route = useRouter();
@@ -7,6 +11,29 @@ export default function Search() {
     query: { q },
   } = route;
 
-  console.log(q);
-  return <p>Search</p>;
+  const { products } = useProducts("created");
+
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const search = q?.toLowerCase();
+    const filter = products.filter(
+      (product) =>
+        product.name.toLowerCase().includes(search) ||
+        product.description.toLowerCase().includes(search)
+    );
+    setResult(filter);
+  }, [q, products]);
+
+  return (
+    <div className={styles.listProducts}>
+      <div className={styles.container}>
+        <ul className={styles.bgWhite}>
+          {result.map((product) => (
+            <ProductDetails key={product.id} product={product} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
