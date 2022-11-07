649a818544ff12205c273ed4994cbee625dd98de
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-07 20:11:31 +0100

    Add fonts and normalize.css in the app's global style file

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,19 +6,6 @@ const Layout = ({ children }) => {
     <>
       <Head>
         <title>Product Hunt Clone - NextJS y Firebase</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <Header />
