af5a1abcb641f1ccb942fb0a28e847126f4e611d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-07 17:45:38 +0100

    Define the popular page using the hook useProducts

--- pages/popular.js
+++ pages/popular.js
@@ -1,3 +1,19 @@
+import ProductDetails from "../components/ProductDetails";
+import useProducts from "../hooks/useProducts";
+import styles from "../styles/Home.module.css";
+
 export default function Popular() {
-  return <p>Popular</p>;
+  const { products } = useProducts("votes");
+
+  return (
+    <div className={styles.listProducts}>
+      <div className={styles.container}>
+        <ul className={styles.bgWhite}>
+          {products.map((product) => (
+            <ProductDetails key={product.id} product={product} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
